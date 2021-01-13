189abc7028dc50a3270aecb114574d86d5d2eaf0
amitznati-work-hp <amitznati@gmail.com>
2021-01-13 20:25:39 +0200

    prepare for prod

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
+import { ApolloServer } from 'apollo-server';
+import dotenv from 'dotenv'
+// import express from 'express';
 import mongoose from 'mongoose';
 import {typeDefs} from './typeDefs'
 import {resolvers} from './resolvers';
 import path from 'path'
 import {createFilesRoutes} from './fileManager';
 
-
+dotenv.config();
 const startServer = async () => {
-	const server = new ApolloServer({	typeDefs, resolvers	});
+	const server = new ApolloServer
+	({
+			typeDefs,
+			resolvers,
+			subscriptions: { path: '/' }
+		});
 
-	const app = express();
-	createFilesRoutes(app);
-	server.applyMiddleware({ app });
-	mongoose.set('useNewUrlParser', true);
-	mongoose.set('useFindAndModify', false);
-	mongoose.set('useCreateIndex', true);
-	mongoose.set('useUnifiedTopology', true);
-	console.log(path.join(__dirname, '../public'));
-	app.use(express.static(path.join(__dirname, '../public')));
-	await mongoose.connect('mongodb://localhost:27017/brand-it', {useNewUrlParser: true, useUnifiedTopology: true});
+	// const uri = 'mongodb+srv://amitznati:Nvrcungahl26@cluster0.smxxf.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&ssl=true';
+	const uri = process.env.DB_URL; //'mongodb://localhost:27017/brand-it';
 
-	app.listen({ port: 4000 }, () => {
-		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-
+	await mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		serverSelectionTimeoutMS: 5000
+	}).catch(err => console.log(err));
+	server.listen().then(({ url, subscriptionsUrl }) => {
+		console.log(`🚀 Server ready at ${url}`)
+		console.log(`🚀 Susbscription ready at ${subscriptionsUrl}`)
 	});
-
 };
 
 startServer();
