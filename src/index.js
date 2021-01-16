c9858b5df6b90d5e3ae25bf59ce815dfb5ae9225
amitznati-work-hp <amitznati@gmail.com>
2021-01-17 00:23:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import { ApolloServer } from 'apollo-server';
+import { ApolloServer as ApolloServerExpress } from 'apollo-server-express';
 import dotenv from 'dotenv'
-// import express from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import {typeDefs} from './typeDefs'
 import {resolvers} from './resolvers';
-// import {createFilesRoutes} from './fileManager/localFileManager';
+import {createFilesRoutes} from './fileManager/localFileManager';
 import "regenerator-runtime/runtime.js";
+import path from 'path';
 
 dotenv.config();
 
-const startServer = async () => {
+const startServerProd = async () => {
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		subscriptions: { path: '/' }
+		subscriptions: { path: '/' },
+		introspection: true
 	});
-
-	// const app = express();
-	// if (process.env.NODE_ENV === 'development') {
-	// 	createFilesRoutes(app);
-	// }
-	// server.applyMiddleware({app, path: '/'});
 
 	await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -30,4 +27,23 @@ const startServer = async () => {
 	});
 }
 
-startServer();
+const startServerDev = async () => {
+	const server = new ApolloServerExpress({ typeDefs, resolvers });
+
+	const app = express();
+	createFilesRoutes(app);
+	server.applyMiddleware({ app });
+	mongoose.set('useNewUrlParser', true);
+	mongoose.set('useFindAndModify', false);
+	mongoose.set('useCreateIndex', true);
+	mongoose.set('useUnifiedTopology', true);
+
+	await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+	app.listen({ port: 4000 }, () => {
+		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+
+	});
+}
+
+startServerProd();
