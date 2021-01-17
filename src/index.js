27e04f6969b6768d3c965963a017202dca1f8652
amitznati-work-hp <amitznati@gmail.com>
2021-01-17 23:52:02 +0200

    test express-graphql

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { ApolloServer } from 'apollo-server';
 import { ApolloServer as ApolloServerExpress } from 'apollo-server-express';
 import dotenv from 'dotenv'
 import express from 'express';
@@ -7,32 +6,23 @@ import {typeDefs} from './typeDefs'
 import {resolvers} from './resolvers';
 import {createFilesRoutes} from './fileManager/localFileManager';
 import "regenerator-runtime/runtime.js";
-import path from 'path';
 
 dotenv.config();
+const isDev = process.env.NODE_ENV === 'development';
 
-const startServerProd = async () => {
-	const server = new ApolloServer({
+const startServerDev = async () => {
+	const server = new ApolloServerExpress({
 		typeDefs,
 		resolvers,
 		subscriptions: { path: '/' },
 		introspection: true
 	});
 
-	await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-
-	server.listen().then(({ url, subscriptionsUrl }) => {
-		console.log(`🚀 Server ready at ${url}`)
-		console.log(`🚀 Susbscription ready at ${subscriptionsUrl}`)
-	});
-}
-
-const startServerDev = async () => {
-	const server = new ApolloServerExpress({ typeDefs, resolvers });
-
 	const app = express();
-	createFilesRoutes(app);
-	server.applyMiddleware({ app });
+	server.applyMiddleware({ app, path: isDev ? '/graphql' : '/' });
+	if (isDev) {
+		createFilesRoutes(app);
+	}
 	mongoose.set('useNewUrlParser', true);
 	mongoose.set('useFindAndModify', false);
 	mongoose.set('useCreateIndex', true);
@@ -42,8 +32,57 @@ const startServerDev = async () => {
 
 	app.listen({ port: 4000 }, () => {
 		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+		console.log(`🚀 subscriptionsPath ready at http://localhost:4000${server.subscriptionsPath}`);
 
 	});
 }
 
-startServerProd();
+const startServerExpress = () => {
+	const express = require('express');
+	const {graphqlHTTP} = require('express-graphql');
+	const { graphqlUploadExpress, GraphQLUpload } = require('graphql-upload');
+	const {
+		GraphQLSchema,
+		GraphQLObjectType,
+		GraphQLBoolean,
+
+	} = require('graphql');
+
+	const schema = new GraphQLSchema({
+		mutation: new GraphQLObjectType({
+			name: 'Mutation',
+			fields: {
+				uploadImage: {
+					description: 'Uploads an image.',
+					type: GraphQLBoolean,
+					args: {
+						image: {
+							description: 'Image file.',
+							type: GraphQLUpload,
+						},
+					},
+					async resolve(parent, { image }) {
+						const { filename, mimetype, createReadStream } = await image;
+						const stream = createReadStream();
+						// Promisify the stream and store the file, then…
+						console.log('stream read!!');
+						return true;
+					},
+				},
+			},
+		}),
+	});
+
+	express()
+		.use(
+			isDev ? '/graphql' : '/',
+			graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
+			graphqlHTTP({schema}))
+		.listen({ port: 4000 }, () => {
+			console.log(`🚀 Server ready at http://localhost:4000`);
+			// console.log(`🚀 subscriptionsPath ready at http://localhost:4000${server.subscriptionsPath}`);
+
+		});
+}
+
+startServerDev();
